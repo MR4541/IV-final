0ebe4e896dca69d9c41e8bdc4efef5886aff124d
MR4541 <jjooyy1872@gmail.com>
2026-06-04 08:50:31 +0800

    refactor: update serializer with new schema in #2

--- src/manager/include/serializer.hpp
+++ src/manager/include/serializer.hpp
@@ -21,7 +21,6 @@
 // Vehicle: data of one vehicle
 typedef struct {
     // input (read from JSON)
-    int vid;                    // vehicle id
     int arrival_time;
     int source_lane;
     int destination_lane;
@@ -30,7 +29,10 @@ typedef struct {
     std::vector<int> schedule;  // time to visit each conflict zone
 } Vehicle;
 
-enum EdgeType {TYPE1, TYPE2, TYPE3};
+// Point: 2D point for the position of a conflict zone
+typedef struct Point{
+    double x, y;
+} Point;
 
 // Data: all we need to perform intersection management
 typedef struct Data{
@@ -38,7 +40,8 @@ typedef struct Data{
     
     int zone_pass; // vertex passing time
     std::vector<int> edge_wait; // edge waiting time[3]
-    int N; // number of conflict zones
+    int N; // number of conflict zone
+    std::vector<Point> zone_pos_list; // position of all zones
 
     // info of vehicles
     
--- src/manager/src/serializer.cpp
+++ src/manager/src/serializer.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <fstream>
 #include <algorithm>
+#include <vector>
 #include "json.hpp"
 #include "serializer.hpp"
 
@@ -8,11 +9,16 @@ using json = nlohmann::json;
 
 // helper function to read array from JSON
 void from_json(const json& j, Vehicle& v){
-    j.at("vid").get_to(v.vid);
     j.at("src_lane").get_to(v.source_lane);
-    j.at("dest_lane").get_to(v.destination_lane);
+    j.at("dst_lane").get_to(v.destination_lane);
     j.at("path").get_to(v.zones);
     j.at("arrival").get_to(v.arrival_time);
+}
+
+void from_json(const json& j, Point& p){
+    std::vector<double> vec = j.get<std::vector<double>>();
+    p.x = vec[0];
+    p.y = vec[1];
 }
 
 // read input from a JSON file and initialize this structure
@@ -22,9 +28,10 @@ void Data::readInput(std::string inputFile){
     
     this->zone_pass = inputData["zone_passing_time"];
     this->edge_wait = inputData["edge_waiting_time"].get<std::vector<int>>();
-    this->N = inputData["n_zones"];
-    this->M = inputData["n_vehicles"];
+    this->zone_pos_list = inputData["zones"].get<std::vector<Point>>();
+    this->N = this->zone_pos_list.size();
     this->vehicles = inputData["vehicles"].get<std::vector<Vehicle>>();
+    this->M = this->vehicles.size();
     // sort vehicles by arrival_time in ascending order
     std::sort(this->vehicles.begin(), this->vehicles.end(),
             [](const Vehicle& a, const Vehicle& b){
@@ -40,20 +47,25 @@ void Data::readInput(std::string inputFile){
 // helper function to write vector to JSON
 void to_json(json& j, const Vehicle& v){
     j = json{
-        {"vid", v.vid},
         {"src_lane", v.source_lane},
-        {"dest_lane", v.destination_lane},
+        {"dst_lane", v.destination_lane},
         {"arrival", v.arrival_time},
         {"path", v.zones},
         {"schedule", v.schedule}
     };
 }
 
+void to_json(json& j, const Point& p){
+    j = json{p.x, p.y}; // [Number, Number]
+}
+
 // write the generated schedule to an output JSON file
 void Data::writeOutput(std::string outputFile){
     json outputData;
+    outputData["zone_passing_time"] = this->zone_pass;
+    outputData["edge_waiting_time"] = this->edge_wait;
+    outputData["zones"] = this->zone_pos_list;
     outputData["vehicles"] = this->vehicles;
-    outputData["n_vehicles"] = this->M;
 
     std::ofstream f(outputFile);
     f << outputData.dump(2); // indent = 2
@@ -65,16 +77,21 @@ void Data::printContent(){
             "%s"
             "N: %d, M: %d\n"
             "zone_pass: %d, edge_wait: %d %d %d\n"
-            "vehicles:\n",
+            "zone_positions:\n\t",
             this->is_scheduled? "" : "not scheduled yet!\n",
             this->N, this->M,
             this->zone_pass, this->edge_wait[0],
             this->edge_wait[1], this->edge_wait[2]);
+    for(const auto& p : this->zone_pos_list)
+        printf("(%f, %f), ", p.x, p.y);
+    int counter = 0;
+    printf("\nvehicles:\n");
     for(const auto& car : this->vehicles){
-        printf("[vid %3d]\n"
+        counter++;
+        printf("[%3d]\n"
                 "\tarrive_time: %d, src: %d, dest: %d\n"
                 "\tzones:",
-                car.vid, car.arrival_time, car.source_lane, 
+                counter, car.arrival_time, car.source_lane, 
                 car.destination_lane);
         int path_len = car.zones.size();
         for(int i = 0; i < path_len; i++){
